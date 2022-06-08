32af841d6be8b48678d95a22981ca4d893ea259e
arturmingaleev <art.mingaleev@yandex.ru>
2022-06-08 18:53:26 +0300

    калькулятор урок 1

--- app/src/main/java/ru/geegbrains/lesson1/MainActivity.java
+++ app/src/main/java/ru/geegbrains/lesson1/MainActivity.java
@@ -1,13 +1,12 @@
 package ru.geegbrains.lesson1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,27 +14,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_main);
 
-        EditText editTextTop = findViewById(R.id.et_top);
-        EditText editTextBottom = findViewById(R.id.et_bottom);
-        Button button = findViewById(R.id.btn_button);
-        TextView textView = findViewById(R.id.tv_result);
+//        EditText editTextTop = findViewById(R.id.et_top);
+//        EditText editTextBottom = findViewById(R.id.et_bottom);
+//        Button button = findViewById(R.id.btn_button);
+//        TextView textView = findViewById(R.id.tv_result);
 
-        button.setOnClickListener(v -> {
-            try {
-                Integer val1 = Integer.valueOf(editTextTop.getText().toString());
-                Integer val2 = Integer.valueOf(editTextBottom.getText().toString());
-
-                if (val1.equals(val2)) {
-                    textView.setText("Равно");
-                } else {
-                    textView.setText("Не равно!!");
-            }
-
-            } catch (NumberFormatException e) {
-                textView.setText("Поля не могут быть пустыми!!!123");
-            }
-        });
+//        button.setOnClickListener(v -> {
+//            try {
+//                Integer val1 = Integer.valueOf(editTextTop.getText().toString());
+//                Integer val2 = Integer.valueOf(editTextBottom.getText().toString());
+//
+//                if (val1.equals(val2)) {
+//                    textView.setText("Равно");
+//                } else {
+//                    textView.setText("Не равно");
+//                }
+//            } catch (NumberFormatException e) {
+//                textView.setText("Поля не могут быть пустыми");
+//            }
+//        });
     }
 }
