4be726a8e8ce4fd326ddb4046359b1e6657ddcea
Артур Мингалеев <arturmingaleev@MacBook-Pro-Artur.local>
2022-06-08 11:15:08 +0300

    Initial commit

---
+++ app/src/main/java/ru/geegbrains/lesson1/MainActivity.java
@@ -0,0 +1,42 @@
+package ru.geegbrains.lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        EditText editTextTop = findViewById(R.id.et_top);
+        EditText editTextBottom = findViewById(R.id.et_bottom);
+        Button button = findViewById(R.id.btn_button);
+        TextView textView = findViewById(R.id.tv_result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Integer val1 = Integer.valueOf(editTextTop.getText().toString());
+                    Integer val2 = Integer.valueOf(editTextBottom.getText().toString());
+
+                    if (val1.equals(val2)) {
+                        textView.setText("Равно");
+                    } else {
+                        textView.setText("Не равно");
+                }
+
+                } catch (NumberFormatException e) {
+                    textView.setText("Поля не могут быть пустыми");
+                }
+            }
+        });
+    }
+}
